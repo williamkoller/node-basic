b268285512082903a471afe90cf2dfa868df11b4
William Koller <wkoller25@gmail.com>
2020-02-16 21:50:10 -0300

    fix

---
+++ file.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+// file system 
+// fs
+//     .writeFile('data.txt', 'Hello World', function(err) {
+//         if (err) {
+//             throw err;
+//         }
+//     });
+
+// fs
+//     .readFile('data.txt', function(err, data) {
+//         if (err) {
+//             throw err;
+//         }
+
+//         console.log(data.toString('utf8'));
+//     });
